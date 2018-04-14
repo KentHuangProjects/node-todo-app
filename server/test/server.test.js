13697ca2336f09cf1414faaf8ec958ef7c5118b6
Kent Huang <kenthuang426@gmail.com>
2018-04-14 14:57:09 +0800

    added test for DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -112,3 +112,46 @@ describe('GET /todos/:id', () => {
         .end(done)
     })
 })
+
+
+describe('DELETE /todos/:id', () => {
+    it('should remove the todo', (done) => {
+        var hexId = todosSample[1]._id.toHexString()
+
+        request(app).delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todosSample[1].text)
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err)
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist()
+                    done()
+                }).catch((e) => done(e))
+            })
+            
+
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        var hid = new ObjectID().toHexString()
+        request(app)
+            .delete(`/todos/${hid}`)
+            .expect(404)
+            .end(done)
+
+    })
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done)
+
+    })
+})
+
+
